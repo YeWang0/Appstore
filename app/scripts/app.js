3d3cc8755495cc22e3cdeefbde608297ddc28815
YeWang0 <yewang0910@gmail.com>
2016-10-21 18:26:38 -0400

    bob loblaw's github account is about to get snazzy

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appstoreApp
+ * @description
+ * # appstoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appstoreApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appstoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appstoreApp
+ */
+angular.module('appstoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
