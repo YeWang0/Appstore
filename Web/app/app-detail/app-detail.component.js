409113fd12a0d9e484f049db1fd2435176504226
YeWang0 <yewang0910@gmail.com>
2016-12-21 11:59:15 -0500

    fix bug

--- Web/app/app-detail/app-detail.component.js
+++ Web/app/app-detail/app-detail.component.js
@@ -6,9 +6,21 @@ angular.
       function($routeParams,$rootScope) {
         this.appId = $routeParams.appId;
         this.apps=$rootScope.apps;
-        this.selectedApp=$rootScope.selectedApp;
-        console.log(this.selectedApp);
-        console.log(this.appId);
+        // console.log($rootScope.apps);
+        if(this.appId==$rootScope.selectedApp.app_id)
+          this.selectedApp=$rootScope.selectedApp;
+        else{
+          var i=0;
+          while(i<$rootScope.apps.length){
+              if($rootScope.apps[i].app_id==this.appId){
+                this.selectedApp=$rootScope.apps[i];
+                break;
+              }
+              i++;
+          }
+        }
+        // console.log(this.selectedApp);
+        // console.log(this.appId);
       }
     ]
   });
